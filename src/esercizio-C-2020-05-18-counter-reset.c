4b1ed9277d6134831d6c94dd09ad50cbce159078
Spopoi <61315723+Spopoi@users.noreply.github.com>
2020-05-26 14:49:42 +0200

    Update esercizio-C-2020-05-18-counter-reset.c

--- src/esercizio-C-2020-05-18-counter-reset.c
+++ src/esercizio-C-2020-05-18-counter-reset.c
@@ -3,73 +3,68 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-
 #include <errno.h>
+#include <semaphore.h>
 #include <pthread.h>
 
+#define CHECK_ERR(a,msg) {if ((a) == -1) { perror((msg)); exit(EXIT_FAILURE); } }
 
 #define LIMIT 100000
 #define RESET_LIMIT 100
 
-
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-// variabili condivise
+sem_t *process_semaphore;
 int counter;
 int reset;
 
-void * thread_function(void * arg) {
-	int s;
-
-	int op1_counter = 0;
-	int op2_counter = 0;
+void* thread_function(void *arg) {
 
 	while (1) {
 
-		s = pthread_mutex_lock(&mutex);
-		if (s != 0) {
-			perror("pthread_mutex_lock");
+		if (reset > RESET_LIMIT-1) {
+			printf("limite raggiunto\n");
+			break;
+		}
+
+		if (sem_wait(process_semaphore) == -1) {
+			perror("sem_wait");
 			exit(EXIT_FAILURE);
 		}
 
-	    counter++;
-	    op1_counter++;
-
-	    if (counter > LIMIT) {
-	        counter = counter - LIMIT;
-	        reset++;
-	        op2_counter++;
-	    }
-
-		s = pthread_mutex_unlock(&mutex);
-		if (s != 0) {
-			perror("pthread_mutex_lock");
-			exit(EXIT_FAILURE);
+		if (counter > LIMIT) {
+			counter = counter - LIMIT;
+			reset++;
 		}
 
-	    if (reset > RESET_LIMIT) {
-	    	printf("op1_counter = %d, op2_counter = %d\n", op1_counter, op2_counter);
-	        return NULL;
-	    }
+		counter++;
+
+		if (sem_post(process_semaphore) == -1) {
+			perror("sem_post");
+			exit(EXIT_FAILURE);
+		}
 
 	}
 
+	return NULL;
 }
 
-
-
-int main() {
+int main(int argc, char *argv[]) {
 
 	pthread_t t1;
 	pthread_t t2;
-	void * res;
+	void *res;
 
 	int s;
+
+	printf("initial value of counter=%d, reset=%d\n", counter, reset);
+
+	process_semaphore = malloc(sizeof(sem_t));
+
+	s = sem_init(process_semaphore, 0, // 1 => il semaforo è condiviso tra processi,
+									   // 0 => il semaforo è condiviso tra threads del processo
+			1 // valore iniziale del semaforo (se mettiamo 0 che succede?)
+			);
+
+	CHECK_ERR(s, "sem_init")
 
 	s = pthread_create(&t1, NULL, thread_function, NULL);
 
@@ -84,8 +79,6 @@ int main() {
 		perror("pthread_create");
 		exit(EXIT_FAILURE);
 	}
-
-
 	s = pthread_join(t1, &res);
 
 	if (s != 0) {
@@ -100,9 +93,14 @@ int main() {
 		exit(EXIT_FAILURE);
 	}
 
-	printf("final value of counter=%d reset=%d\n", counter, reset);
+	printf("final value of counter=%d\n & value of reset: %d\n", counter,
+			reset);
+
+	// il semaforo senza nome va distrutto solo quando non ci sono processi bloccati su di esso
+	s = sem_destroy(process_semaphore);
+	CHECK_ERR(s, "sem_destroy")
 
 	printf("bye\n");
 
-	return 0;
 }
+
